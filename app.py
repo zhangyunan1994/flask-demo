8728d1b2a2e6518f43cd38b09763f77412d9ede7
zhangyunan <zyndev@gmail.com>
2021-01-29 11:23:27 +0800

    增加登录页面

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+from flask_cors import *
+
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 
 
 @app.route('/')
-def hello_world():
+def index():
+    # render_template 为使用模板渲染对应的页面并跳转到页面，对应的页面在 templates 下面
     return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    # 此处应该判断一下，如果已经登录，就不要跳到登录页面了
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
---
+++ config.py
@@ -0,0 +1,52 @@
+import configparser
+
+
+class Server:
+    RunMode: str
+    HttpPort: int
+
+    def __init__(self, run_mode: str, http_port: int):
+        self.RunMode = run_mode
+        self.HttpPort = http_port
+
+
+class Database:
+    User: str
+    Password: str
+    Host: str
+    Name: str
+
+    def __init__(self, host: str, user: str, password: str, database: str) -> None:
+        self.Host = host
+        self.User = user
+        self.Password = password
+        self.Name = database
+
+
+cfg = configparser.ConfigParser()
+
+cfg.read('conf/app.ini')
+
+
+def get_dict(section: str) -> dict:
+    result = {}
+    for item in cfg.items(section):
+        result[item[0]] = item[1]
+    return result
+
+
+def get_server_info() -> Server:
+    info = get_dict("server")
+    return Server(info.get('runmode'), info.get('httpport'))
+
+
+def get_database_info(env: str) -> Database:
+    info = get_dict("database_" + env)
+    return Database(info.get('host'), info.get('user'), info.get('password'), info.get('name'))
+
+
+if __name__ == '__main__':
+    print(cfg.items("server"))
+    print(get_dict("server"))
+    print(get_server_info().HttpPort)
+    print(get_database_info("test").Host)
